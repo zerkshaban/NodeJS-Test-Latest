ba6bf8dd444ec7bf9cbbccc086ee7418b4d3b8b6
zerkshaban <60327014+zerkshaban@users.noreply.github.com>
2020-03-28 20:57:12 +0500

    Converting the code to function.

--- Problem#2/api/routes/country.js
+++ Problem#2/api/routes/country.js
@@ -2,15 +2,17 @@ const express = require('express');
 const router = express.Router();
 const fetch = require("node-fetch");
 
-// Added hardcoded country just for testing
-var country = 'malta';
 
-//Api to featch the information of the given country
-router.get('/', async (request ,response)=>{
-  const api_url = `https://restcountries.eu/rest/v2/name/${country}`;
-  const fetch_response = await fetch(api_url);
-  const json = await fetch_response.json();
-  response.json(json);
-})
+getCountryData = (country)=>{
+  //Api to featch the information of the given country
+  router.get('/', async (request ,response)=>{
+    const api_url = `https://restcountries.eu/rest/v2/name/${country}`;
+    const fetch_response = await fetch(api_url);
+    const json = await fetch_response.json();
+    response.json(json);
+  })
+}
+
+getCountryData('malta');
 
 module.exports = router;
